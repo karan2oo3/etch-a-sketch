595bc5ad04f31fe33388bdb520f4f5af5177af97
karan2oo3 <kk645ssrw@gmail.com>
2024-10-04 12:40:49 +0530

    working with the buttons and coloring

--- main.js
+++ main.js
@@ -1,19 +1,65 @@
+let color = "black"
+
 document.addEventListener("DOMContentLoaded" ,function() {
-    createBoard(32);
-    console.log("hi")
+    createBoard(16);
+    let btnPopup = document.querySelector("#popup");
+    btnPopup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
+
 })
 
 function createBoard(size){
     let board = document.querySelector(".board")
 
-    board.style.grindTemplateColumns = 'repeat(${size}, 1fr)'
-    board.style.gridTemplateRows = 'repeat(${size}, 1fr)'
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
     
     let numDivs =size * size;
+    board.innerHTML="";
     for( let i =0; i<numDivs;i++){
         let div = document.createElement("div")
-        div.style.backgroundColor = "yellow"
+     div.addEventListener("mouseover", colorDiv)
         board.insertAdjacentElement("beforeend",div)
 
     }
+}
+
+
+function getSize(){
+    let input = prompt("Whats the size of the board ? ")
+    let message = document.querySelector("#message")
+    if(input== "")
+    {
+        message.innerHTML=  "please provide a number";
+    }
+    else if(input < 1 || input > 100){
+        message.innerHTML = "please provide a number between 1 and 100"
+    }
+    else {
+        message.innerHTML = "Now you can play"
+        return input;
+    }
+}
+
+function colorDiv(){
+
+    if(color ==="random"){
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
+   }
+    else 
+    {
+        this.style.backgroundColor = 'black'
+    }
+
+}
+
+function setColor(colorChoice){
+ color = colorChoice;
+
+}
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach(div => div.style.backgroundColor = 'white')
 }
